c001608a49b5fc8c35f20bbd35d09d6d48d91310
Рудольф Багмет <nolde3@yandex.ru>
2021-09-20 02:22:34 +0400

    added test for Drag-n-Drop

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 Репозиторий для обучения по автоматизированному тестированию на Java
 
 **Урок 2.**
-Тест для https://demoqa.com/
+Тест для https://demoqa.com/ - AutomationPracticeForm
 
+**Урок 4.**
+1. Тесты для GitHub - GithubTests
+2. Тест на drag-and-drop - TheInternetApp
---
+++ src/test/java/ru/bagmet/TheInternetApp.java
@@ -0,0 +1,17 @@
+package ru.bagmet;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TheInternetApp {
+
+    @Test
+    void TestDragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("div#column-a>header").shouldHave(text("B"));
+    }
+
+}
